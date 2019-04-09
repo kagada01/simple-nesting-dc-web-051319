d38b4d4e71183b8c1074d3a25cfa3d6641d17504
Dan Kagan <dankagan7@gmail.com>
2019-04-09 21:44:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,5 +136,5 @@ def adding_to_dennis
      
 programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 
-programmer_hash[:dennis_ritchie][:languages]
+return programmer_hash[:dennis_ritchie][:languages]
 end
